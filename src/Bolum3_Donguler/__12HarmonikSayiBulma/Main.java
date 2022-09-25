dc954836d78237aa7184d2667fece31cfcd4e766
tolgauzan <tolga_uzan@hotmail.com>
2022-09-25 05:50:35 +0300

    upload

--- src/Bolum3_Donguler/__12HarmonikSayiBulma/Main.java
+++ src/Bolum3_Donguler/__12HarmonikSayiBulma/Main.java
@@ -1,7 +1,26 @@
 package Bolum3_Donguler.__12HarmonikSayiBulma;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        Java ile girilen sayının harmonik serisini bulan program
+         yazacağız.
+
+        Harmonik Seri Formülü :
+        1+(1/2)+(1/3)+(1/3)+(1/4)+..(1/n)
+         */
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Harmonik Hesap\nSayi girin");
+        int sayi=scanner.nextInt();
+        double sonuc=0;
+        for(double i=1;i<=sayi;i++){
+            sonuc+=(1/i);
+        }
+        System.out.println(sonuc);
     }
 }
