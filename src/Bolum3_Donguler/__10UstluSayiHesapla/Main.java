eb303a346c3cea27b5eea1d171e9d83beb8216c4
tolgauzan <tolga_uzan@hotmail.com>
2022-09-25 04:29:41 +0300

    upload

--- src/Bolum3_Donguler/__10UstluSayiHesapla/Main.java
+++ src/Bolum3_Donguler/__10UstluSayiHesapla/Main.java
@@ -1,7 +1,34 @@
 package Bolum3_Donguler.__10UstluSayiHesapla;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        /*
+        Java ile kullanıcının girdiği değerler ile üslü sayı
+        hesaplayan programı yazıyoruz.
+
+        Ödev
+        Java ile kullanıcının girdiği değerler ile üslü sayı
+        hesaplayan programı "For Döngüsü" kullanarak yapınız.
+         */
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sayi,ussu;
+        System.out.println("Üssü alınacak sayı ;");
+        sayi = scanner.nextInt();
+        System.out.println("Üs sayi ; ");
+        ussu = scanner.nextInt();
+
+        int sonuc=1;
+        //3^4= 3*3*3*3 tür.
+        for(int i=1;i<=ussu;i++){
+            sonuc*=sayi;
+        }
+        System.out.println(sonuc);
 
     }
 }
